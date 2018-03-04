f4df51c070d0513e812f409153fcc766b7bee337
bhalder <barun@pensando.io>
2018-03-03 17:25:45 -0800

    Enqueue/Dequeue implementation

--- producer_consumer.c
+++ producer_consumer.c
@@ -13,56 +13,67 @@
 
 /* Queue structure - Circular Queue at that */
 struct queue {
-	unsigned int head;
-	unsigned int tail;
-	unsigned int data[MAXQUEUE];	
+  unsigned int head;
+  unsigned int tail;
+  unsigned int data[MAXQUEUE];  
+};
+
+struct queue ringBuffer;
+
+/* Queue full condition is : head & MASK == tail & MASK && head > tail */ 
+void enqueue(unsigned int d) {
+  if((ringBuffer.head & MASK) == (ringBuffer.tail & MASK) &&
+     (ringBuffer.head > ringBuffer.tail)) {
+    printf ("Queue full! Dropping data [%u]", d);
+    return;
+  } else {
+    ringBuffer.head++;
+    ringBuffer.data[ringBuffer.head & MASK] = d; 
+  }
 }
 
-
-/* Queue full condition is : head == tail */ 
-void enqueue() {
-
-	/* TODO: Add code */
-
-}
-
-/* Queue empty condition is : (head & MASK) - (tail & MASK) == MAXQUEUE */
+/* Queue empty condition is : head == tail */
 unsigned int dequeue() {
-
-	/* TODO: Add code */
-
+  if(ringBuffer.head == ringBuffer.tail) {
+    printf("Queue is empty. No data to be shared.");
+    return 0;
+  } else {
+    unsigned int d = ringBuffer.data[ringBuffer.tail & MASK];
+    ringBuffer.tail++;
+    return d;
+  }
 }
 
 /* Function to "produce" aka. enqueue() random numbers */
 void producerWork() {
 
-	/* TODO: Add code */
+  /* TODO: Add code */
 
 }
 
 /* Function to "consume" aka. dequeue() numbers from the queue */
 void consumerWork() {
 
-	/* TODO: Add code */
+  /* TODO: Add code */
 
 }
 
 /* The function which the producer thread runs continuously */
 void producer() {
 
-	/* TODO: Add code */
+  /* TODO: Add code */
 
 }
 
 void consumer() {
 
-	/* TODO: Add code */
+  /* TODO: Add code */
 
 }
 
 int main() {
-	pthread_t producer[PRODUCERCOUNT];
-	pthread_t consumer[CONSUMERCOUNT];
+  pthread_t producer[PRODUCERCOUNT];
+  pthread_t consumer[CONSUMERCOUNT];
 
-	return 0;	
+  return 0;  
 }
